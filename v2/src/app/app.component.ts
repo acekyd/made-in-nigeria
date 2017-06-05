5182dd113a5072656ff1164fa21f9becd047d00d
Theophilus Omorebee <theo4u@ymail.com>
2017-06-05 22:49:31 +0100

    added modal for brief info

--- v2/src/app/app.component.ts
+++ v2/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  showInfo: boolean = true;
 }
