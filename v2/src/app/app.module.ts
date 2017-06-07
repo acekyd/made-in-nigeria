88f1fbf5aa454cbfc464beed9eab83874c9c420b
Theophilus Omorebee <theo4u@ymail.com>
2017-06-07 10:34:53 +0100

    separated each repo to be a component kinda directive

--- v2/src/app/app.module.ts
+++ v2/src/app/app.module.ts
@@ -7,11 +7,13 @@ import {ClarityModule} from 'clarity-angular';
 import {AppComponent} from './app.component';
 import { MainComponent } from './main/main.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ v2/src/app/repository/repository.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {IRepository} from "../interfaces/irepository";
+
+@Component({
+  selector: 'repository',
+  template: `<div class="naija-card">
+    <div class="naija-header">
+        <img src="https://avatars0.githubusercontent.com/u/10440327?v=3">
+        <a href="#" target="_blank">{{repo.name}}</a>
+
+        <a href="#" target="_blank" role="tooltip" aria-haspopup="true"
+           class="tooltip tooltip-sm tooltip-left pull-right"
+           style="font-size:12px">
+            <clr-icon shape="user"></clr-icon>
+            {{repo.creator}}
+            <span class="tooltip-content">Creator</span>
+        </a>
+
+        <p>
+            {{repo.description}}
+        </p>
+    </div>
+    <div class="naija-card-footer">
+        <a *ngFor="let stack of repo.stacks" style="text-decoration: none"
+           href="https://en.wikipedia.org/wiki/JavaScript" target="_blank">
+            <img class="stack-icon" src="assets/{{stack}}.svg"
+                 alt="js"></a>
+    </div>
+</div><!--/naija-card-->
+`,
+  styleUrls: ['./repository.component.css']
+})
+export class RepositoryComponent implements OnInit {
+  @Input() repo: IRepository;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
