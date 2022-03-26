766dfa9a5100e223d354ad6e7b3344f959c81e52
Adewale Abati <acekyd01@gmail.com>
2022-03-26 20:44:41 +0100

    Delete previous web version

--- src/app/pipes/search-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchFilterPipe } from './search-filter.pipe';
-
-describe('SearchFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
