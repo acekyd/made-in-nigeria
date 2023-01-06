eb21900dc38267bb5a1d44fec755e62589c1bf97
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-01-06 17:13:54 +0100

    navbar component - first draft

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import NavBar from '../components/NavBar';
 import ProjectCard from '../components/ProjectCard';
 
 /*
@@ -18,6 +19,7 @@ const IndexPage = () => {
           <p>The journey to Made in Nigeria V2 begins.</p>
           <Link to="/about">About Made in Nigeria </Link>
         </main>
+        <NavBar />
         <ProjectCard />
       </ChakraProvider>
     </Layout>
