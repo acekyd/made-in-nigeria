179ef2695e0b90e3c22d44a4d338220e35422a6a
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-04-15 17:23:00 +0100

    alphabet simplegrid components

---
+++ src/components/AlphabetFilter.js
@@ -0,0 +1,166 @@
+import { Box, SimpleGrid, Text } from '@chakra-ui/react';
+import React from 'react';
+
+const AlphabetFilter = () => {
+  return (
+    <SimpleGrid columns={{ base: 8, md: 26 }} rowGap={6} background="#FFFFFF" pt="2rem">
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem">
+        <Text color="#B8BAB9" fontWeight="800">
+          A
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          B
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          C
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          D
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          E
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          F
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          G
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          H
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          I
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          J
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          K
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          L
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          M
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          N
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          O
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          P
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          Q
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          R
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          S
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          T
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          U
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          V
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          W
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          X
+        </Text>
+      </Box>
+
+      <Box borderLeft="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          Y
+        </Text>
+      </Box>
+
+      <Box borderX="1px solid #E2E3E3" padding="0.5rem 1rem" margin="0 !important">
+        <Text color="#B8BAB9" fontWeight="800">
+          Z
+        </Text>
+      </Box>
+    </SimpleGrid>
+  );
+};
+
+export default AlphabetFilter;
