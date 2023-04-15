09db12a9e7675a9d74db442efb0faa4d2693d2dd
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-04-15 17:22:11 +0100

    render project cards

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,18 +1,46 @@
-import * as React from "react"
+import * as React from 'react';
+import Layout from '../components/layout';
+import ProjectsHero from '../components/ProjectsHero';
+import { Box, ChakraProvider, Container, SimpleGrid } from '@chakra-ui/react';
+import AlphabetFilter from '../components/AlphabetFilter';
+import ProjectCard from '../components/ProjectCard';
 
 /*
   Notice: This is going to be the listing page for all projects
 */
 
-
 // markup
 const ProjectsPage = () => {
-    return (
-        <main>
-            <title>Projects</title>
-            <p>The journey to Made in Nigeria V2 begins.</p>
-        </main>
-    )
-}
+  return (
+    <Layout>
+      <ChakraProvider>
+        <Container maxW="container.xl" centerContent top>
+          <Box my={{ base: '3rem', md: '7rem' }}>
+            <ProjectsHero />
+          </Box>
 
-export default ProjectsPage
+          <Box position="sticky" top="90" zIndex={100}>
+            <AlphabetFilter />
+          </Box>
+
+          <SimpleGrid columns={{ sm: 1, md: 3 }} mt="1rem" mb="5rem">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+          </SimpleGrid>
+        </Container>
+      </ChakraProvider>
+    </Layout>
+  );
+};
+
+export default ProjectsPage;
