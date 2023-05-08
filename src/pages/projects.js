5b5e58a99e1cb096a14af814d460f815492771d3
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-08 16:41:19 +0100

    rendering work in progress

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,8 +2,10 @@ import * as React from 'react';
 import Layout from '../components/layout';
 import ProjectsHero from '../components/ProjectsHero';
 import { Box, ChakraProvider, Container, SimpleGrid } from '@chakra-ui/react';
-import AlphabetFilter from '../components/AlphabetFilter';
+import AlphabetFilterNormal from '../components/AlphabetFilterNormal';
 import ProjectCard from '../components/ProjectCard';
+import AlphabetFilterStuck from '../components/AlphabetFilterStuck';
+import AlphabetFilterExpand from '../components/AlphabetFilterExpand';
 
 /*
   Notice: This is going to be the listing page for all projects
@@ -19,8 +21,16 @@ const ProjectsPage = () => {
             <ProjectsHero />
           </Box>
 
+          <Box>
+            <AlphabetFilterStuck />
+          </Box>
+
+          <Box>
+            <AlphabetFilterExpand />
+          </Box>
+
           <Box position="sticky" top="90" zIndex={1}>
-            <AlphabetFilter />
+            <AlphabetFilterNormal />
           </Box>
 
           <SimpleGrid columns={{ sm: 1, md: 3 }} mt="1rem" mb="5rem">
