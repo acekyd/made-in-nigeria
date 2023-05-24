4ebd16a693c8b604a738261774684f234745da48
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-24 13:18:24 +0100

    dynamic alphabet generation for the component and code optimization

--- src/components/AlphabetFilter/AlphabetFilterStuck.js
+++ src/components/AlphabetFilter/AlphabetFilterStuck.js
@@ -3,95 +3,36 @@ import { Button, Center, Flex } from '@chakra-ui/react';
 import React from 'react';
 
 const AlphabetFilterStuck = ({ isExpanded, setIsExpanded }) => {
+  const stuckAlphabetsCharCodes = Array.from(Array(7)).map((element, index) => index + 65);
+  const stuckAlphabetsStrings = stuckAlphabetsCharCodes.map((charCode) =>
+    String.fromCharCode(charCode)
+  );
+
+  const filterProjects = (letter) => {
+    // filter function goes here
+    console.log(`filtering by ${letter}`);
+  };
+
   const expand = () => {
     setIsExpanded(true);
   };
   return (
     <Center borderBottom="0.5px solid #E2E3E3" pb="1rem" width="100vw" background="#FFFFFF">
       <Flex gap="3px">
-        <Center borderLeft="0.5px solid #E2E3E3" padding="0 0.2rem">
-          <Button
-            background="none"
-            padding={0}
-            color="#B8BAB9"
-            fontWeight="800"
-            _hover={{ background: '#f4f4f4' }}
-          >
-            A
-          </Button>
-        </Center>
-
-        <Center borderLeft="0.5px solid #E2E3E3" padding="0 0.2rem">
-          <Button
-            background="none"
-            padding={0}
-            color="#B8BAB9"
-            fontWeight="800"
-            _hover={{ background: '#f4f4f4' }}
-          >
-            B
-          </Button>
-        </Center>
-
-        <Center borderLeft="0.5px solid #E2E3E3" padding="0 0.2rem">
-          <Button
-            background="none"
-            padding={0}
-            color="#B8BAB9"
-            fontWeight="800"
-            _hover={{ background: '#f4f4f4' }}
-          >
-            C
-          </Button>
-        </Center>
-
-        <Center borderLeft="0.5px solid #E2E3E3" padding="0 0.2rem">
-          <Button
-            background="none"
-            padding={0}
-            color="#B8BAB9"
-            fontWeight="800"
-            _hover={{ background: '#f4f4f4' }}
-          >
-            D
-          </Button>
-        </Center>
-
-        <Center borderLeft="0.5px solid #E2E3E3" padding="0 0.2rem">
-          <Button
-            background="none"
-            padding={0}
-            color="#B8BAB9"
-            fontWeight="800"
-            _hover={{ background: '#f4f4f4' }}
-          >
-            E
-          </Button>
-        </Center>
-
-        <Center borderLeft="0.5px solid #E2E3E3" padding="0 0.2rem">
-          <Button
-            background="none"
-            padding={0}
-            color="#B8BAB9"
-            fontWeight="800"
-            _hover={{ background: '#f4f4f4' }}
-          >
-            F
-          </Button>
-        </Center>
-
-        <Center borderLeft="0.5px solid #E2E3E3" padding="0 0.2rem">
-          <Button
-            background="none"
-            padding={0}
-            color="#B8BAB9"
-            fontWeight="800"
-            _hover={{ background: '#f4f4f4' }}
-          >
-            G
-          </Button>
-        </Center>
+        {stuckAlphabetsStrings.map((letter) => (
+          <Center key={letter} borderLeft="0.5px solid #E2E3E3" padding="0 0.2rem">
+            <Button
+              background="none"
+              padding={0}
+              color="#B8BAB9"
+              fontWeight="800"
+              _hover={{ background: '#f4f4f4' }}
+              onClick={() => filterProjects(letter)}
+            >
+              {letter}
+            </Button>
+          </Center>
+        ))}
 
         <Center borderLeft="0.5px solid #E2E3E3" padding="0 0.2rem">
           <Button background="none" padding={0} _hover={{ background: '#f4f4f4' }} onClick={expand}>
