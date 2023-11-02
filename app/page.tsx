de50f443837c4d1891da85c4ff52307a4a484fb0
Adewale Abati <acekyd01@gmail.com>
2023-11-02 09:15:36 +0100

    First experiments

--- app/page.tsx
+++ app/page.tsx
@@ -14,16 +14,11 @@ async function getData() {
   }
 
   const markdownData = await res.text();
-
   const html = marked(markdownData);
-
-  // write a script to parse html string to select all ul > li elements
-  // and then create an array of objects with the data
-
   const $ = cheerio.load(html); // load the html string into cheerio
 
   // Select all <li> elements using jQuery-like syntax and extract their text
-const liTextArray = $('li').map((index, element) => $(element).html()).get();
+  const liTextArray = $('li').map((index, element) => $(element).html()).get();
 
 
   console.log("Wahala", liTextArray);
