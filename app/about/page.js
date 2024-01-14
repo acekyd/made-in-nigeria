044901c59b0cc44283e653592de449edda7c601f
kaf-lamed-beyt <belac335@gmail.com>
2024-01-14 11:35:00 +0100

    fix(ui): refactor NavBar

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,8 @@
 export default function About() {
-    return (
-        <main>
-            <title>About</title>
-            <p>The journey to Made in Nigeria V2 begins.</p>
-        </main>
-    )
+  return (
+    <main>
+      <title>About</title>
+      <p>The journey to Made in Nigeria V2 begins.</p>
+    </main>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Providers } from './providers';
-import NavBar from './components/Navbar'
-import Footer from './components/Footer'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Providers } from "./providers";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Made in Nigeria',
-  description: 'A curation of awesome open source tools built by Nigerians for the world.',
-}
+  title: "Made in Nigeria",
+  description:
+    "A curation of awesome open source tools built by Nigerians for the world.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -27,5 +28,5 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
-  )
+  );
 }
