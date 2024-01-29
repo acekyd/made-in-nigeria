77365d69ae75c899935f15c1399d9ddeeeddb8b6
kaf-lamed-beyt <belac335@gmail.com>
2024-01-29 14:18:24 +0100

    refactor: move from a store persistence approach to a reusable helper function

--- app/components/pages/HomePage.tsx
+++ app/components/pages/HomePage.tsx
@@ -1,10 +1,10 @@
+import { useProjects } from "@/app/utils/projects";
 import { Home } from "./components/homepage-client";
 import { getArticles } from "@/app/utils/mdx";
-import { getData } from "@/app/projects/page";
 
 export default async function HomePage() {
   const articles = await getArticles();
-  const projects = await getData();
+  const { projects } = await useProjects();
 
   return <Home data={articles} projects={projects} />;
 }
--- app/projects/page.js
+++ app/projects/page.js
@@ -1,63 +1,13 @@
-import { marked } from "marked";
-import * as cheerio from "cheerio";
 import ProjectsPage from "../components/pages/ProjectsPage";
-
-export async function getData() {
-  const res = await fetch(
-    "https://raw.githubusercontent.com/acekyd/made-in-nigeria/master/README.MD"
-  );
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  const markdownData = await res.text();
-  const html = marked(markdownData);
-  const $ = cheerio.load(html); // load the html string into cheerio
-
-  // Select all <li> elements using jQuery-like syntax and extract their text
-  const liTextArray = $("li")
-    .map((index, element) => $(element).html())
-    .get();
-
-  // process the text to get the data you want
-  const repositories = convertToJSON(liTextArray);
-
-  return repositories;
-}
-
-function convertToJSON(repositories) {
-  return repositories.map((repository) => {
-    const $ = cheerio.load(repository);
-
-    // Extract text content and href from <a> element
-    const repoName = $("a").first().text();
-    const repoLink = $("a").first().attr("href");
-
-    let description = $("*").contents()[3].data; // I don't know why the fuck this works but if it's not broken, don't touch it.
-    const repoDescription = description.replace(/^ - /, "");
-    const repoAuthor = $("strong a").text();
-    const repoAuthorLink = $("strong a").attr("href");
-
-    // Create JSON object
-    return {
-      repoName,
-      repoLink,
-      repoDescription,
-      repoAuthor,
-      repoAuthorLink,
-    };
-  });
-}
+import { useProjects } from "../utils/projects";
 
 export default async function Projects() {
-  const data = await getData();
+  const { projects } = await useProjects();
 
   return (
     <main>
       <title>Projects | Made In Nigeria</title>
-      <ProjectsPage repositories={data} />
+      <ProjectsPage repositories={projects} />
     </main>
   );
 }
