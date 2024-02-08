880603a91823b11ebe472c9169f188d56869053c
Adewale Abati <acekyd01@gmail.com>
2024-02-08 10:59:31 +0100

    Clean for build

--- app/about/page.js
+++ app/about/page.js
@@ -4,7 +4,7 @@ import AboutPage from "../components/pages/AboutPage";
 import data from "../assets/team.json";
 import React, {useEffect} from "react";
 
-export default async function About() {
+export default function About() {
   return (
     <main>
       <title>About | Made In Nigeria</title>
