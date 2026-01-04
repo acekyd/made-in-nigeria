22c0ebe5de7245e31f0c7501eda1406a1b4109a2
Bashir Hamza <bashirhamza05@gmail.com>
2026-01-04 21:37:52 +0100

    fix: updated to fix issue for overflowing box (#342)
    * fix: updated to fix issue for overflowing box
    
    * remove yarn lock

--- app/components/Blog/ArticleCard.tsx
+++ app/components/Blog/ArticleCard.tsx
@@ -21,8 +21,11 @@ const ArticleCard = ({ image, slug, title, excerpt }: CardProps) => {
         maxWidth="80vw"
         w={{ sm: "21rem", lg: "23.7rem" }}
         height="30.75rem"
+        overflow="hidden"
+        display="flex"
+        flexDirection="column"
       >
-        <Flex flexDirection="column" mx={4} my={4} gap={3}>
+        <Flex flexDirection="column" mx={4} my={4} gap={3} flex={1} minHeight={0}>
           <Image
             src={image}
             borderRadius="8px 8px 0 0"
@@ -30,18 +33,16 @@ const ArticleCard = ({ image, slug, title, excerpt }: CardProps) => {
             height="280px"
             objectFit="cover"
             objectPosition={{ lg: "top", base: "top" }}
+            flexShrink={0}
           />
-          <Text
-            color="#292F2E"
-            fontWeight={800}
-            lineHeight="tall"
-            fontSize="lg"
-          >
+          <Text color="#292F2E" fontWeight={800} fontSize="lg" noOfLines={2}>
             {title}
           </Text>
-          <Text fontSize="sm" lineHeight="taller" color="#949796">
-            {excerpt}
-          </Text>
+          <Box flex={1} minHeight={0} overflow="hidden">
+            <Text fontSize="sm" color="#949796" noOfLines={4}>
+              {excerpt}
+            </Text>
+          </Box>
         </Flex>
       </Box>
     </Link>
